e254a246afa1dcaa18b6bdb9279e576397d6e892
sushi1694 <124113986+sushi1694@users.noreply.github.com>
2023-03-01 13:47:53 +0530

    Create ch.c

---
+++ ch.c
@@ -0,0 +1,16 @@
+ #include <stdio.h>
+ int main()
+ {
+ 	int num1,num2;
+ 	printf("Enter 1st number : ");
+ 	scanf("%d",&num1);
+ 	printf("Enter 2nd number : ");
+ 	scanf("%d",&num2);
+ 	if(num1==num2)
+ 	{printf("Number 1 and number 2 are equal");
+	 }
+	else
+	{printf("Number 1 is not equal to number 2");
+	 }
+	 return 0;
+ }
